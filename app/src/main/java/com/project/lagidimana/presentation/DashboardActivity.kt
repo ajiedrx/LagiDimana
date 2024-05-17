3e815fb2fa8215240c6302ce9cf2d0760224c9e8
ajiedr <ajiedibyor@gmail.com>
2024-05-17 17:15:27 +0700

    Refactor code

--- app/src/main/java/com/project/lagidimana/presentation/DashboardActivity.kt
+++ app/src/main/java/com/project/lagidimana/presentation/DashboardActivity.kt
@@ -81,20 +81,6 @@ class DashboardActivity : ComponentActivity() {
         checkLocationPermission()
     }
 
-    @Deprecated("This method has been deprecated in favor of using the Activity Result API\n      which brings increased type safety via an {@link ActivityResultContract} and the prebuilt\n      contracts for common intents available in\n      {@link androidx.activity.result.contract.ActivityResultContracts}, provides hooks for\n      testing, and allow receiving results in separate, testable classes independent from your\n      activity. Use\n      {@link #registerForActivityResult(ActivityResultContract, ActivityResultCallback)} passing\n      in a {@link RequestMultiplePermissions} object for the {@link ActivityResultContract} and\n      handling the result in the {@link ActivityResultCallback#onActivityResult(Object) callback}.")
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            MY_PERMISSIONS_REQUEST_LOCATION -> {
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_DENIED) {
-                    showLocationPermissionRequestDeniedDialog()
-                    Toast.makeText(this, "Permission denied", Toast.LENGTH_LONG).show()
-                }
-                return
-            }
-        }
-    }
-
     private fun checkLocationPermission() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             requestLocationPermission()
